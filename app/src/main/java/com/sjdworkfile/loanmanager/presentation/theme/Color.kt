67745ef15e622b40a437c07ca03f1e8d335fb5cd
sajjad hosseini <sjdworkfile@gmail.com>
2026-06-01 13:05:08 +0330

    Add Color definitions

---
+++ app/src/main/java/com/sjdworkfile/loanmanager/presentation/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.sjdworkfile.loanmanager.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light colors
+val Primary = Color(0xFF6200EE)
+val Secondary = Color(0xFF03DAC6)
+val Tertiary = Color(0xFF03DAC6)
+val Background = Color(0xFFFAFAFA)
+val Surface = Color(0xFFFFFFFF)
+
+// Dark colors
+val PrimaryDark = Color(0xFFBB86FC)
+val SecondaryDark = Color(0xFF03DAC6)
+val TertiaryDark = Color(0xFF03DAC6)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1F1F1F)
+
+// Additional colors
+val Success = Color(0xFF4CAF50)
+val Error = Color(0xFFB00020)
+val Warning = Color(0xFFFFC107)
+val Info = Color(0xFF2196F3)
+
+val SuccessDark = Color(0xFF81C784)
+val ErrorDark = Color(0xFFEF5350)
+val WarningDark = Color(0xFFFFD54F)
+val InfoDark = Color(0xFF64B5F6)
